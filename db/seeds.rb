5bc7b7e11fdbbcd4cf44e0e94e36367f02513583
miguelmassv <miguelmassv@hotmail.com>
2018-06-04 14:00:15 -0500

    listo

---
+++ db/seeds.rb
@@ -0,0 +1,12 @@
+Category.create(name: "Perfumes")
+Category.create(name: "Libros")
+Category.create(name: "Tecnologia")
+Category.create(name: "Electrodomesticos")
+Category.create(name: "Aseo")
+Category.create(name: "Juguetes")
+Category.create(name: "Ropa")
+Category.create(name: "Oficina")
+Category.create(name: "Mascotas")
+
+Product.create(name: "bicileta", price: 123456, category_ids: [1,2,3,4])
+Product.create(name: "pc", price: 123, category_ids: [5,6,7,8])
